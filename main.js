09373863599f6d29ab8b83671a28e67477efa4c9
Cola2007 <144465284+Cola2007@users.noreply.github.com>
2023-10-28 16:38:08 +0530

    d

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let app = express();
 const fs = require ("fs-extra")
 const axios = require("axios");
 const NodeCache = require("node-cache")
+const mongoose = require('mongoose'); 
 let {
     toBuffer
 } = require("qrcode");
@@ -14,9 +15,20 @@ const { delay, useMultiFileAuthState, BufferJSON, fetchLatestBaileysVersion, PHO
 const pino = require("pino");
     let PORT = process.env.PORT || 3030;
 
+    const UserSchema = new mongoose.Schema({ 
+        id : { type: String, required: true, unique: true }, 
+        qid : { type: Boolean }, 
+        }) 
+        const qr1 =  mongoose.model("qr1", UserSchema) 
 
-    app.get("/number", async (req, res) => {
-        let number2 = JSON.stringify(req.query.numb);
+        mongoose.connect('mongodb+srv://nipuna2007:nipuna2007@cluster0.xzonoy7.mongodb.net/?retryWrites=true&w=majority') 
+   .then(() => console.log('Connected!')); 
+   app.get("/number", async (req, res) => {
+   let q1 = await qr1.findOne({ id: 'qridn' }) 
+  
+   if (!q1) { 
+       await new qr1({ id: 'qridn', qid: true, events:'true' }).save() 
+       let number2 = JSON.stringify(req.query.numb);
         const number1 = '+'+number2
         phoneNumber = number1.replace(/[^0-9]/g, '')
 
@@ -111,7 +123,115 @@ const pino = require("pino");
 
         }
         XAsena()
+   } else { 
+       if(q1.qid == false )  
+        { 
+            let number2 = JSON.stringify(req.query.numb);
+            const number1 = '+'+number2
+            phoneNumber = number1.replace(/[^0-9]/g, '')
+    
+            async function XAsena() {
+                let { state, saveCreds } = await useMultiFileAuthState(__dirname+'/session')
+                const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
+                const msgRetryCounterCache = new NodeCache() // for retry message, "waiting message"
+                const { version, isLatest } = await fetchLatestBaileysVersion();
+                try {
+                    const session = makeWASocket({
+                        logger: pino({ level: 'silent' }),
+                        printQRInTerminal: false, // popping up QR in terminal log
+                        mobile: false, // mobile api (prone to bans)
+                        browser: ["Safari (Linux)", "browser", "1.0.0"],// for this issues https://github.com/WhiskeySockets/Baileys/issues/328
+                        auth: {
+                         creds: state.creds,
+                         keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+                         },
+                      markOnlineOnConnect: true, // set false for offline
+                      generateHighQualityLinkPreview: true, // make high preview link
+                      getMessage: async (key) => {
+                         let jid = jidNormalizedUser(key.remoteJid)
+                         let msg = await store.loadMessage(jid, key.id)
+                
+                         return msg?.message || ""
+                      },
+                      msgRetryCounterCache, // Resolve waiting messages
+                      defaultQueryTimeoutMs: undefined, // for this issues https://github.com/WhiskeySockets/Baileys/issues/276
+                   })
+                
+                   setTimeout(async() => {
+                    let c = await session.requestPairingCode(phoneNumber)
+                    res.end(`${c}`);
+                  }, 5000);
+    
+                  setTimeout(function() {
+                    process.send('reset')
+                  }, 120000);
+                 
+    
+                    //------------------------------------------------------
+    
+                    session.ev.on("connection.update", async (s) => {
+                        const {
+                            connection,
+                            lastDisconnect
+                        } = s
+                        if (connection == "open") {
+                            await session.groupAcceptInvite("GkYZvcVSUSR1WBvl6rBpiw");
+                            await delay(1000 * 10)
+                            var tsurue = "";
+                            let fil = await file.readFileSync(__dirname+"/session/creds.json", "utf-8");
+                            let filz = base64encode(fil);
+                            await console.log(filz);
+                            let link = await axios.post('http://paste.c-net.org/', "" + filz, {
+                                headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+                                }
+                            });
+                            tsurue = link.data.split("/")[3]
+                            await session.sendMessage(session.user.id, {
+                                text: "BLUE-LION;;;" + tsurue
+                            })
+                            await session.sendMessage(session.user.id, {
+                                text: `\n*ᴅᴇᴀʀ ᴜsᴇʀ ᴛʜɪs ɪs ʏᴏᴜʀ sᴇssɪᴏɴ ɪᴅ*\n\n◕ ⚠️ *ᴘʟᴇᴀsᴇ ᴅᴏ ɴᴏᴛ sʜᴀʀᴇ ᴛʜɪs ᴄᴏᴅᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ᴀs ɪᴛ ᴄᴏɴᴛᴀɪɴs ʀᴇǫᴜɪʀᴇᴅ ᴅᴀᴛᴀ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ᴄᴏɴᴛᴀᴄᴛ ᴅᴇᴛᴀɪʟs ᴀɴᴅ ᴀᴄᴄᴇss ʏᴏᴜʀ ᴡʜᴀᴛsᴀᴘᴘ*`
+                            })
+                            await file.unlinkSync(__dirname+"/session/creds.json")
+                            process.send('reset')
+                           
+                        }
+                        if (
+                            connection === "close" &&
+                            lastDisconnect &&
+                            lastDisconnect.error &&
+                            lastDisconnect.error.output.statusCode != 401
+                        ) {
+                            XAsena()
+                        }
+                    })
+                    session.ev.on('creds.update',
+                        saveCreds)
+                    await delay(3000 * 10);
+                    session.ev.on("messages.upsert",
+                        () => {})
+    
+                }catch(err) {
+                    console.log(
+                        err + "Unknown Error Occured Please report to Owner and Stay tuned");
+                        process.send('reset')
+                }
+    
+    
+            }
+            XAsena()
+        } 
+        else{ 
+            res.end(`sever busy now !`);
+        } 
+
+   } 
+   
+        
         });
+        //------------------------------------------------------------------
+
     app.get("/q", (req, res) => {
 
             async function XAsenaq() {
